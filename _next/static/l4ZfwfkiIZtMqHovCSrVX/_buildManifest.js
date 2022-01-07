ca94cb3a3eef7f5314265aa3f19b180b84cae2cd
ameerhmzx <ameerhmzx@gmail.com>
2022-01-07 13:24:08 +0000

    Deploying to gh-pages from @ The-Advertizers/LAATA_qualify@d53c6d2460ccaf55c4293d66f89147e7537fab76 🚀

--- _next/static/l4ZfwfkiIZtMqHovCSrVX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cb1608f2-51f2391aa918143e.js","static/chunks/724-2e1c59eeaef617d9.js","static/chunks/pages/index-1c590921f844f213.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
